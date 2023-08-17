1927b64b1a4d36ff681f6a13092c1eb8d9d1402b
Jeff Tibbetts <jtibbetts@preload.com>
2023-08-17 16:38:07 -0400

    Add Post Deploy Script

---
+++ CollectionsDb/Script.PostDeployment.sql
@@ -0,0 +1,21 @@
+﻿if not exists (select 1 from dbo.[Person])
+begin
+	INSERT INTO [dbo].[Person]
+	(
+		[FirstName], 
+		[LastName], 
+		[MiddleName], 
+		[PreferredName], 
+		[DateOfBirth], 
+		[IsActive]
+	)
+	VALUES
+	(
+		'Jeffrey', 
+		'Tibbetts', 
+		'Allen', 
+		'Jeff', 
+		'1973-05-29', 
+		1
+	);
+end
